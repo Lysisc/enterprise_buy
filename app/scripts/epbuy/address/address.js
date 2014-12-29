62b42b6a6856956ebeb46a0aefceb2e850ecd65d
若青 <lysisc@192.168.0.7>
2014-12-30 01:21:08 +0800

    modify

--- app/scripts/epbuy/address/address.js
+++ app/scripts/epbuy/address/address.js
@@ -6,7 +6,7 @@ angular.module('EPBUY')
         $scope.isChoice = $state.is('epbuy.choice');
 
         Util.ajaxRequest({
-            url: '$local/GetHomeRestaurantBannerInfo.json',
+            url: '$local/GetHomeRestaurantBannerInfo.json1',
             data: {
                 enterpriseCode: $scope.enterpriseCode // todo...
             },
@@ -62,8 +62,6 @@ angular.module('EPBUY')
 
             if ($scope.editable) {
                 obj = type ? $scope.enterpriseList[type] : $scope.personageList[index];
-            } else {
-                return;
             }
 
             if ($scope.isChoice && !angular.isNumber(isAdd)) {
